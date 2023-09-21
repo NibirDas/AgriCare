9fb3fd2d4c9f3f2c9f95662d293b83c6d51bcdcc
Nibir Das <das579nibir@gmail.com>
2023-09-22 05:08:13 +0530

    Version 1.0

---
+++ app/src/main/java/com/example/agricare/SpWithAnimationActivity.java
@@ -0,0 +1,30 @@
+package com.example.agricare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.os.Handler;
+
+public class SpWithAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp_with_animation);
+
+        Handler handler = new Handler();
+
+        Runnable delayedIntentRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // Create and start your intent here
+                Intent intent = new Intent(SpWithAnimationActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+        handler.postDelayed(delayedIntentRunnable, 4000);
+    }
+
+}
